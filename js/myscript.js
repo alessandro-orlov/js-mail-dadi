64f078f5c0be8e64d0b1b09b93b4e580f06edb9e
Alessandro <alex.ixs@hotmail.com>
2020-05-25 15:54:44 +0200

    Esercizio 2 completato

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
-//ESERCIZIO 1
+//ESERCIZIO 1: Lista email
 
 //Lista Email (Array)
 var listaEmail = ["alessandro@mail.com", "andrea@mail.com", "damiano@mail.com", "laura@mail.com", "marco@mail.com", "alex.ixs@mail.com"];
@@ -25,5 +25,23 @@ if(accessGranted) {
 } else {
   alert('You shall not pass!')
 }
-
 // FINE ESERCIZIO 1
+
+//ESERCIZIO 2: DATI
+//Genera il numero per l'utente
+var userNumber = Math.floor(Math.random() * 6) + 1;
+alert('il numero dell\'utente è: ' + userNumber);
+
+// Genera il numero per il computer
+var computerNumber = Math.floor(Math.random() * 6) + 1;
+alert('il numero del computer è: ' + computerNumber);
+
+// Verifica il vincitore in base al numero maggiore
+if(userNumber > computerNumber) {
+  alert('Ha vinto l\'utente');
+} else if (userNumber < computerNumber) {
+  alert('Ha vinto il computer');
+} else {
+  alert('DRAW');
+}
+// FINE ESERCIZIO 2
