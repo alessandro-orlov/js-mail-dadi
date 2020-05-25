d463dd7712030858d4b06824edb1def8a09da902
Alessandro <alex.ixs@hotmail.com>
2020-05-25 14:56:38 +0200

    Email list created

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,5 @@
-alert('hello')
+var listaEmail = ["alessandro@mail.com", "andrea@mail.com", "damiano@mail.com", "laura@mail.com", "marco@mail.com", "alex.ixs@mail.com"];
+console.log(listaEmail);
+
+var tuaEmail = prompt('Qual\'è la tua Email');
+console.log(tuaEmail);
