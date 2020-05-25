b6da8a5920ba102223781dfb8740aaa17c87cfee
Alessandro <alex.ixs@hotmail.com>
2020-05-25 15:39:11 +0200

    Exercise 1 fix
    Verifica console log per vedere chi ha accesso

--- js/myscript.js
+++ js/myscript.js
@@ -2,22 +2,25 @@
 
 //Lista Email (Array)
 var listaEmail = ["alessandro@mail.com", "andrea@mail.com", "damiano@mail.com", "laura@mail.com", "marco@mail.com", "alex.ixs@mail.com"];
+for ( var i = 0; i < listaEmail.length; i++ ) {
+  console.log(listaEmail[i]);
+}
 
 //Variabile sulla quale effettuare la verifica
-var specialMail = false;
+var accessGranted = false;
 
 // Chiede l'email dell'utente
-var tuaEmail = prompt('Qual\'è la tua Email');
+var tuaEmail = prompt('Qual\'è la tua Email? *see console.log');
 
 // Verifica la lista (Cicle)
 for ( var i = 0; i < listaEmail.length; i++ ) {
   if(listaEmail[i] === tuaEmail) {
-    specialMail = true;
+    accessGranted = true;
   }
 }
 
 //Da accesso se l'email utente è presente sulla lista
-if(specialMail) {
+if(accessGranted) {
   alert('Access granted');
 } else {
   alert('You shall not pass!')
