239d1e40217b7b0573052f350cf5c2810fc3aba5
Alessandro <alex.ixs@hotmail.com>
2020-05-25 15:30:27 +0200

    Exercise 1 complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,26 @@
+//ESERCIZIO 1
+
+//Lista Email (Array)
 var listaEmail = ["alessandro@mail.com", "andrea@mail.com", "damiano@mail.com", "laura@mail.com", "marco@mail.com", "alex.ixs@mail.com"];
-// console.log(listaEmail);
 
-// var tuaEmail = prompt('Qual\'è la tua Email');
-// console.log(tuaEmail);
+//Variabile sulla quale effettuare la verifica
+var specialMail = false;
 
-for ( var i = 0; i < listaEmail.lenght; i++ ) {
-  console.log(listaEmail[i]);
+// Chiede l'email dell'utente
+var tuaEmail = prompt('Qual\'è la tua Email');
+
+// Verifica la lista (Cicle)
+for ( var i = 0; i < listaEmail.length; i++ ) {
+  if(listaEmail[i] === tuaEmail) {
+    specialMail = true;
+  }
 }
+
+//Da accesso se l'email utente è presente sulla lista
+if(specialMail) {
+  console.log('Access granted');
+} else {
+  console.log('You shall not pass!')
+}
+
+// FINE ESERCIZIO 1
